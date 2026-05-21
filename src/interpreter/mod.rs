622c5bb86002e6a3733a6501e3d23d0caed18f59
Lucas Clemente Vella <lvella@powdrlabs.com>
2026-05-21 13:16:09 -0300

    Storing RegisterValue internally.

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -55,8 +55,8 @@ trait RegisterBank {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum RegisterValue {
-    Unassigned,
     Dropped,
+    Unassigned,
     Future(u32),
     Concrete(u32),
 }
@@ -222,7 +222,7 @@ impl RegisterBank for WriteOnceRegisterBank {
 
 struct ReadWriteRegisterBank {
     /// Each register is `Some(value)` if alive, or `None` if dropped.
-    regs: Vec<Option<u32>>,
+    regs: Vec<RegisterValue>,
 }
 
 impl ReadWriteRegisterBank {
@@ -233,18 +233,18 @@ impl ReadWriteRegisterBank {
 
 impl RegisterBank for ReadWriteRegisterBank {
     fn get(&mut self, addr: u32) -> RegisterValue {
-        match self.regs.get(addr as usize) {
-            Some(Some(value)) => RegisterValue::Concrete(*value),
-            Some(None) | None => RegisterValue::Dropped,
-        }
+        self.regs
+            .get(addr as usize)
+            .cloned()
+            .unwrap_or(RegisterValue::Dropped)
     }
 
     fn set(&mut self, addr: u32, value: u32) {
         let addr = addr as usize;
         if addr >= self.regs.len() {
-            self.regs.resize(addr + 1, None);
+            self.regs.resize(addr + 1, RegisterValue::Dropped);
         }
-        self.regs[addr] = Some(value);
+        self.regs[addr] = RegisterValue::Concrete(value);
     }
 
     fn copy_range(&mut self, src: Range<u32>, dest: Range<u32>) {
@@ -271,7 +271,7 @@ impl RegisterBank for ReadWriteRegisterBank {
     fn drop_reg(&mut self, addr: u32) {
         let addr = addr as usize;
         if addr < self.regs.len() {
-            self.regs[addr] = None;
+            self.regs[addr] = RegisterValue::Dropped;
         }
     }
 
